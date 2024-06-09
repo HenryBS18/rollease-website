e14829a560a1227827267532b1bde98dc048fcd5
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2024-06-09 23:42:06 +0800

    Add onClick function for the logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,17 @@
 import PrimaryButton from "./PrimaryButton"
 
 const Navbar = ({ className }: { className?: string }) => {
+  const logoOnClick = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" })
+    window.history.replaceState(null, '', '/')
+  }
+
   return (
     <div className={`w-full h-20 px-12 fixed z-50 bg-white ${className}`}>
       <div className="w-full h-full flex items-center justify-between">
-        <a href="/">
+        <div className="cursor-pointer" onClick={logoOnClick}>
           <img src="/rollease-logo.png" alt="logo" width={56} height={56} />
-        </a>
+        </div>
 
         <div className="flex gap-x-20 items-center">
           <a href="#about-us" className="text-[#768EAA] text-lg font-semibold select-none">About Us</a>
